b5985ae6900027b7c0757fa5d1f05cd1b676a6ef
Lucifer <soumitramandalee3rd330@gmail.com>
2022-07-01 00:35:02 +0530

    Fix_Some_Error

---
+++ Tabile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+printf("Enter The Number Which Table You Want\n");
+scanf("%d", &a);a,
+printf("%d X 1 = %d\n",a,a*1);
+printf("%d X 2 = %d\n",a,a*2);
+printf("%d X 3 = %d\n",a,a*3);
+printf("%d X 4 = %d\n",a,a*4);
+printf("%d X 5 = %d\n",a,a*5);
+printf("%d X 6 = %d\n",a,a*6);
+printf("%d X 7 = %d\n",a,a*7);
+printf("%d X 8 = %d\n",a,a*8);
+printf("%d X 9 = %d\n",a,a*9);
+printf("%d X 10 = %d\n",a,a*10);
+printf("%d X 11 = %d\n",a,a*11);
+printf("%d X 12 = %d\n",a,a*12);
+printf("%d X 13 = %d\n",a,a*13);
+printf("%d X 14 = %d\n",a,a*14);
+printf("%d X 15 = %d\n",a,a*15);
+printf("%d X 16 = %d\n",a,a*16);
+printf("%d X 17 = %d\n",a,a*17);
+printf("%d X 18 = %d\n",a,a*18);
+printf("%d X 19 = %d\n",a,a*19);
+printf("%d X 20 = %d\n",a,a*20);
+    return 0;
+}
